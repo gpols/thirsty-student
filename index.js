4b0e5f0b82d495e3aefd27ba599f819e8f3d3434
gpols <137527777+gpols@users.noreply.github.com>
2023-11-01 12:27:41 +0000

    with_css

--- index.js
+++ index.js
@@ -1,19 +1,27 @@
 // Import express and ejs
 var express = require ('express')
 var ejs = require('ejs')
+var bodyParser= require ('body-parser')
+var path = require('path')
+
 // Create the express application object
 const app = express()
 const port = 8000
+app.use(bodyParser.urlencoded({ extended: true }))
+
+
 // Set the directory where Express will pick up HTML files
 // __dirname will get the current directory
 app.set('views', __dirname + '/views');
 // Tell Express that we want to use EJS as the templating engine
 app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, '/public')))
+
 // Tells Express how we should process html files
 // We want to use EJS's rendering engine
 app.engine('html', ejs.renderFile);
 // define our data
-var shopData = {shopName: "The beer shop",
+var shopData = {shopName: "The beer",
 productCategories:["Beer", "Wine", "Soft Drinks", "Hot Drinks"], shopLocations:["Camden", "New Cross", "Peckham"], managerDetails:["Bob Rob", "172 Garrat Lane"]};
 // Handle our routes
 app.get('/',function(req,res){
@@ -25,6 +33,18 @@ res.render('about.html', shopData);
 app.get('/search',function(req,res){
 res.render("search.html", shopData);
 });
+app.get('/search-result', function (req, res) {
+    // TODO: search in the database
+    res.send("You searched for: " + req.query.keyword);
+ });
+ app.get('/register', function (req,res) {
+    res.render('register.html',shopData);
+});                                                                                                 
+app.post('/registered', function (req,res) {
+    // saving data in database
+    res.send(' Hello '+ req.body.first + ' '+ req.body.last +' you are now registered!'  + ' ' + 'We will send an email to you at' + ' ' + req.body.email);   
+}); 
+
 // Start the web app listening
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
